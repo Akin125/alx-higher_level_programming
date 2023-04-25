37606c6327c7d425633dbf31a74e3755ccf7d50c
Odediran Philip Oluseyi <110476898+Akin125@users.noreply.github.com>
2023-04-25 16:40:36 +0100

    done

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+    if (!error) {
+        const todos = JSON.parse(body);
+        let completed = {};
+        todos.forEach(myTodo);
+
+        function myTodo(todo) {
+            if (todo.completed) {
+                if (completed[todo.userId] === undefined) {
+                    completed[todo.userId] = 1;
+                } else if (todo.completed) {
+                    completed[todo.userId] += 1;
+                }
+            } else if (todo.completed) {
+                completed[todo.userId] += 1;
+            }
+        }
+
+        console.log(completed);
+    }
+});
